2bbf46e213a938fa6907fc941be28846832c44e7
WongKwongYu <12872451+WongKwongYu@users.noreply.github.com>
2025-10-16 13:43:38 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -20,3 +20,5 @@ rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2sock
 git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages
 rm -rf feeds/luci/applications/luci-app-passwall
 git clone https://github.com/xiaorouji/openwrt-passwall package/passwall-luci
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/kenzok8/golang -b 1.25 feeds/packages/lang/golang
