85c2cd448f0d096f4fa8777f8d176691e27b09f7
WongKwongYu <12872451+WongKwongYu@users.noreply.github.com>
2026-01-24 18:26:53 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -20,8 +20,8 @@ sed -i 's/IMG_PREFIX:=/IMG_PREFIX:=$(shell date +"%Y%m%d")-/1' include/image.mk
 #rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
 #git clone https://github.com/sbwml/openwrt_helloworld package/helloworld
 # 更新 golang 1.25 版本
-#rm -rf feeds/packages/lang/golang
-#git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
 
 
 # 移除 openwrt feeds 自带的核心库
